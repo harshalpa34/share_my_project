a9e95fc3e5df8fa87ab30d9dbd01f6c0bed383f4
Harshal Pagare <frontend@eight-at-521574992517>
2024-02-28 11:32:51 +0530

    abc

--- grafbase.config.ts
+++ grafbase.config.ts
@@ -1,26 +1,29 @@
-import { config, g } from "@grafbase/sdk";
+import { g, config,  } from '@grafbase/sdk';
 
-
-const User = g.model("User", {
-  name: g.string().length({ min: 2, max: 20 }),
+// @ts-ignore
+const User = g.model('User', {
+  name: g.string().length({ min: 2, max: 100 }),
   email: g.string().unique(),
   avatarUrl: g.url(),
-  description: g.string(),
+  description: g.string().length({ min: 2, max: 1000 }).optional(),
   githubUrl: g.url().optional(),
-  linkedInUrl: g.url().optional(),
-  projects: g.relation(() => Project.list().optional()),
-});
+  linkedinUrl: g.url().optional(), 
+  projects: g.relation(() => Project).list().optional(),
+})
 
-const Project = g.model("Project", {
+// @ts-ignore
+const Project = g.model('Project', {
   title: g.string().length({ min: 3 }),
-  description: g.string(),
+  description: g.string(), 
   image: g.url(),
-  liveSiteUrl: g.url(),
-  githubUrl: g.url(),
+  liveSiteUrl: g.url(), 
+  githubUrl: g.url(), 
   category: g.string().search(),
   createdBy: g.relation(() => User),
-});
+})
+
 
 export default config({
   schema: g,
-});
+ 
+})
